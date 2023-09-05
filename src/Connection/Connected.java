25397f2619cad3c5fe95d751fe3f78c00d29139e
Mehdi Ez-zahri <mehdiezzahriii@gmail.com>
2023-09-05 11:56:46 +0100

    BT-9 : Creation de la base de donnée et l'intergaction avec elle avec succé

---
+++ src/Connection/Connected.java
@@ -0,0 +1,41 @@
+package Connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public final class Connected {
+    private static final String DB_NAME = "bibliotech";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "";
+    private static final String SERVER_NAME = "localhost";
+    private static Connection con = null;
+
+    private Connected() {
+    }
+
+    public static Connection getConnection() {
+        if (con == null) {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                con = DriverManager.getConnection("jdbc:mysql://" + SERVER_NAME + "/" + DB_NAME, USERNAME, PASSWORD);
+            } catch (SQLException | ClassNotFoundException e) {
+                System.out.println("Erreur de connection avec la base de donnée: " + e);
+            }
+        }
+
+        return con;
+    }
+
+    public static void disconnect() {
+        try {
+            if (con != null && !con.isClosed()) {
+                con.close();
+            }
+        } catch (SQLException e) {
+            System.out.println("Erreur de déconnexion: " + e.getMessage());
+        }
+
+        con = null;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,25 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import  java.sql.*;
+import java.util.Scanner;
+import Connection.Connected;
+
 public class Main {
     public static void main(String[] args) {
+        try (Connection connection = Connected.getConnection()) {
+            String sqlQuery = "SELECT * FROM livre";
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sqlQuery);
 
+            // Traitement du résultat ici
+            while (resultSet.next()) {
+                int columnCount = resultSet.getMetaData().getColumnCount();
+                for (int i = 1; i <= columnCount; i++) {
+                    System.out.print(resultSet.getString(i) + "\t");
+                }
+                System.out.println(); // Nouvelle ligne après chaque ligne
+            }
+            Connected.disconnect();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
